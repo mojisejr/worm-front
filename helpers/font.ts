ca6f6959a9ad945c40ba8cf039df81285c69ce08
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-06-27 22:00:32 +0700

    re: redeploy #2

--- helpers/font.ts
+++ helpers/font.ts
@@ -1,15 +1,15 @@
 import localFont from "@next/font/local";
 
 export const arl = localFont({
-  src: "../public/fonts/ARLRDBD.ttf",
+  src: "/fonts/ARLRDBD.ttf",
   style: "normal",
 });
 export const gum = localFont({
-  src: "../public/fonts/Bubblegum.ttf",
+  src: "/fonts/Bubblegum.ttf",
   style: "normal",
 });
 
 export const kga = localFont({
-  src: "../public/fonts/KGAlwaysAGoodTime.ttf",
+  src: "/fonts/KGAlwaysAGoodTime.ttf",
   style: "normal",
 });
